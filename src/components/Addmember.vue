4db3616f3b2d9f37dc3e7670814707dc392ebbb8
卫鑫 <17317051230@163.com>
2019-04-20 19:05:21 +0800

    first commit

---
+++ src/components/Addmember.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <el-button type="text" @click="centerDialogVisible = true">点击打开 Dialog</el-button>
+    <el-dialog title="新增" :visible.sync="centerDialogVisible" width="30%">
+      <div class="main">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="人员" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="内容" prop="region">
+            <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
+              <el-option label="内容一" value="shanghai"></el-option>
+              <el-option label="内容二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="日期" required prop="date">
+            <el-date-picker
+              v-model="ruleForm.date"
+              range-separator="-"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              type="daterange"
+            ></el-date-picker>
+          </el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "addMember",
+  props:[showAdd],
+  data() {
+    return {
+      centerDialogVisible: false,
+      ruleForm: {
+        name: "",
+        region: "",
+        date: ""
+      },
+      rules: {
+        name: [{ required: true, trigger: "blur" }],
+        region: [{ required: true, trigger: "change" }],
+        date: [{ required: true, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.centerDialogVisible = false;
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  },
+  watch:{
+    centerDialogVisible(){
+      this.$emit("update:isShow",false)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  width: 250px;
+  height: 125px;
+  margin: auto;
+}
+.main >>> .el-form-item__label {
+  text-align: left;
+}
+.main >>> .el-input__inner {
+  height: 26px;
+  width: 200px;
+}
+.main >>> .el-form-item {
+  margin: 0;
+}
+.el-date-editor >>> .el-range__icon {
+  line-height: 0px;
+}
+.el-date-editor >>> .el-range-separator {
+  line-height: 20px;
+  color: #c0c4cc;
+}
+</style>
---
+++ src/views/Enter.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="container">
+    <!-- 头部 -->
+    <the-header></the-header>
+    <div class="main">
+      <div class="contain">
+        <!-- 搜索部分 -->
+        <div class="search">
+          <div class="search-left">
+            <div class="search-item">
+              <div class="date">
+                <span>日</span>
+                <span>期</span>
+              </div>
+              <el-date-picker
+                v-model="date"
+                range-separator="-"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                type="daterange"
+              ></el-date-picker>
+            </div>
+            <div class="search-item">
+              <div class="date">
+                <span>姓</span>
+                <span>名</span>
+              </div>
+              <el-input v-model="name" placeholder="请输入内容"></el-input>
+            </div>
+            <div class="search-item">
+              <div class="date">
+                <span>内</span>
+                <span>容</span>
+              </div>
+              <el-input v-model="contain" placeholder="请输入内容"></el-input>
+            </div>
+            <el-button type="success">查询</el-button>
+            <el-button>清空</el-button>
+          </div>
+          <el-button type="success">新增</el-button>
+        </div>
+        <!-- 表格上面部分 -->
+        <div class="table-handle">
+          <div class="table-handle-left">
+            <span style="margin-right:10px">共2019条</span>
+            <span style="margin-right:10px">已选：1234</span>
+            <span style="margin-right:10px">|</span>
+            <span>
+              <el-button type="danger" plain>批量删除</el-button>
+            </span>
+          </div>
+          <div class="table-handle-right">
+            <img src="../assets/loadLogo.png" alt>
+            <img src="../assets/documentLogo.png" alt>
+            <img src="../assets/downLoadLogo.png" alt>
+          </div>
+        </div>
+        <!-- 表格部分 -->
+        <div class="table">
+          <el-table
+            ref="multipleTable"
+            border
+            :data="tableData"
+            cell-style="padding:0;font-size:12px"
+            tooltip-effect="dark"
+            :header-cell-style="getRowClass"
+            style="width: 100%"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="60"></el-table-column>
+            <el-table-column label="序号" width="120">
+              <template slot-scope="scope">{{ scope.row.id }}</template>
+            </el-table-column>
+            <el-table-column prop="birthday" label="时间" width="340"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="430" show-overflow-tooltip></el-table-column>
+            <el-table-column prop="contain" label="内容" width="750" show-overflow-tooltip></el-table-column>
+            <el-table-column fixed="right" label="操作" width="100">
+              <template slot-scope="scope">
+                <span class="change-btn">
+                  <el-button @click="handleClick(scope.row)" type="text" size="small">修改</el-button>
+                </span>
+                <span class="line-btn">|</span>
+                <span class="delect-btn">
+                  <el-button type="text" size="small">删除</el-button>
+                </span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+        <!-- 分页部分 -->
+        <div class="footer">
+          <el-pagination
+            background
+            layout="sizes,prev, pager, next,total"
+            :page-sizes="[100, 200, 300, 400]"
+            :page-size="100"
+            :total="1000"
+          ></el-pagination>
+        </div>
+      </div>
+    </div>
+    <addMember :show.sync='showAdd'></addMember>
+  </div>
+</template>
+
+<script>
+import theHeader from "../components/Header";
+import addMember from "../components/AddMember";
+import api from "./../axios/api.js";
+
+export default {
+  name: "Enter",
+  components: {
+    theHeader,
+    addMember
+  },
+  data() {
+    return {
+      date: "",
+      name: "",
+      contain: "",
+      tableData: [],
+      multipleSelection: [],
+      showAdd:false
+    };
+  },
+  mounted() {
+    this.getdata();
+  },
+  methods: {
+    getRowClass({ row, column, rowIndex, columnIndex }) {
+      if (rowIndex === 0) {
+        return "background:#eee;height:40px;font-size:12px;color:black；";
+      } else {
+        return "";
+      }
+    },
+    handleClick(row) {
+      console.log(row);
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    getdata() {
+      api.mockdata("/data/index").then(res => {
+        console.log(res);
+        this.tableData = res.list;
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  background-color: #f8f8f8;
+  width: 100%;
+}
+.main {
+  margin-top: 4px;
+  background-color: white;
+  width: 100%;
+}
+.contain {
+  width: 1840px;
+  margin: auto;
+}
+.search {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.search-left {
+  display: flex;
+}
+.search-item {
+  display: flex;
+  margin-right: 40px;
+  align-items: center;
+}
+.date {
+  width: 56px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 25px;
+}
+.el-date-editor {
+  width: 200px;
+  height: 30px;
+}
+.el-date-editor >>> .el-range__icon {
+  line-height: 22px;
+}
+.el-date-editor >>> .el-range-separator {
+  color: #c0c4cc;
+  line-height: 22px;
+}
+.el-input {
+  width: 200px;
+}
+.search-item >>> .el-input__inner {
+  width: 200px;
+  height: 30px;
+}
+.search-left .el-button {
+  height: 30px;
+  line-height: 0px;
+}
+.search .el-button {
+  width: 100px;
+  height: 30px;
+  line-height: 0px;
+}
+.line-btn {
+  color: #c0c4cc;
+}
+.change-btn >>> .el-button--text {
+  color: orange;
+}
+.delect-btn >>> .el-button--text {
+  color: red;
+}
+.table-handle {
+  width: 100%;
+  height: 50px;
+  background-color: #f0f8ff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
+}
+.table-handle-left {
+  margin-left: 12px;
+}
+.table-handle-left .el-button {
+  width: 86px;
+  height: 32px;
+  padding: 0px;
+}
+.table-handle-right img {
+  width: 18px;
+  height: 18px;
+  margin-right: 20px;
+}
+.footer {
+  height: 85px;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.el-date-editor >>> .el-range__close-icon {
+  line-height: 22px;
+}
+</style>
